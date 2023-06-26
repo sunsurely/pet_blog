4bb9c82794083190c8034c528b14961f8c9f7e33
Kim Pillseon <pillsun00@naver.com>
2023-06-26 15:45:13 +0900

    오타수정

--- migrations/20230626050316-create-posts.js
+++ migrations/20230626050316-create-posts.js
@@ -14,7 +14,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         references: {
           model: 'Users', // Users 모델을 참조합니다.
-          key: 'usersId', // Users 모델의 userId를 참조합니다.
+          key: 'userId', // Users 모델의 userId를 참조합니다.
         },
         onDelete: 'CASCADE', // 만약 Users 모델의 userId가 삭제되면, Posts 모델의 데이터가 삭제됩니다.
       },
