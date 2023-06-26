f00b6bdcd05389556ad21d6faa8b2e4fbddbf084
Kim Pillseon <pillsun00@naver.com>
2023-06-26 21:31:39 +0900

    index파일업데이트

--- routes/posts.js
+++ routes/posts.js
@@ -2,11 +2,38 @@ const express = require('express');
 const router = express.Router();
 const { Posts } = require('../models');
 const { Users } = require('../models');
+const { Op } = require('sequelize');
+const fs = require('fs');
+const multer = require('multer');
+const AWS = require('aws-sdk');
 const loginMiddleware = require('../middleware/login-middleware');
+
+const upload = multer({ dest: 'uploads/' });
+
+AWS.config.update({
+  accessKeyId: 'aws엑세스키 입력하세요 꼭 삭제하고 깃헙 올리세요~',
+  sceretAccessKey: '비밀키 깃허브 올릴 때 꼭 삭제해야 됩니다',
+});
+
+const s3 = new AWS.S3();
+
+async function uploadToS3(imageFile) {
+  const fileContent = fs.readFileSync(imageFile.path);
+  const fileName = `${Date.now()}_${imageFile.originalname}`;
+  const params = {
+    Bucket: 'pet_blog',
+    Key: fileName,
+    Body: fileContent,
+    ContentType: imageFile.mimetype,
+  };
+  await s3.upload(params).promise();
+  const imageUrl = `https://pet_blog.s3.amazonaws.com/${fileName}`;
+  return imageUrl;
+}
 
 router.get('/', async (req, res) => {
   const posts = await Posts.findAll({
-    attributes: ['postId', 'title', 'createdAt'],
+    attributes: ['postId', 'imageUrl', 'title', 'createdAt'],
     order: [['createdAt', 'DESC']],
   });
 
@@ -18,6 +45,7 @@ router.get('/:postId', async (req, res) => {
   try {
     const post = await Posts.findOne({
       attributes: [
+        'imageUrl',
         'postId',
         'title',
         'nickname',
@@ -33,10 +61,13 @@ router.get('/:postId', async (req, res) => {
   }
 });
 
-router.post('/', loginMiddleware, async (req, res) => {
+router.post('/', loginMiddleware, upload.single('image'), async (req, res) => {
+  const imageFile = req.file;
+  const imageUrl = await uploadToS3(imageFile);
   const { userId } = res.locals.user;
   const { title, content } = req.body;
   const post = await Posts.create({
+    imageUrl,
     UsersId: userId,
     nickname: user.nickname,
     title,
@@ -46,6 +77,49 @@ router.post('/', loginMiddleware, async (req, res) => {
   return res.status(201).json({ data: post });
 });
 
-module.exports = router;
+router.put('/:postId', loginMiddleware, async (req, res) => {
+  const { usersId } = res.locals.user;
+  const { postId } = req.params;
+  const { title, content } = req.body;
 
-//
+  const post = await Posts.findOne({
+    where: { [Op.and]: [{ postId }, { usersId }] },
+  });
+
+  if (!post) {
+    return res
+      .status(400)
+      .json({ errorMessage: '게시물을 수정할 수 없습니다.' });
+  }
+
+  await Posts.update(
+    { title, content },
+    {
+      where: {
+        [Op.and]: [{ postId }, { usersId }],
+      },
+    },
+  );
+
+  res.status(201).json({ message: '게시물을 수정했습니다.' });
+});
+
+router.delete('/:postId', loginMiddleware, async (req, res) => {
+  const { userId } = res.locals.user;
+  const { postId } = req.params;
+  const post = await Posts.findOne({
+    where: { [Op.and]: [{ postId }, { usersId }] },
+  });
+
+  if (!post) {
+    return res.status(400).json({
+      sucess: false,
+      errorMessage: '게시글의 삭제 권한이 존재하지 않습니다.',
+    });
+  }
+  await Posts.destroy({ where: { postId } });
+
+  res.status(200).json({ message: '게시글을 삭제했습니다.' });
+});
+
+module.exports = router;
