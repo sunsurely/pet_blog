e8a60d5699c6f7ca7bee046d8dd4156220c09085
jiahnkim230502 <locker9417@gmail.com>
2023-06-26 19:35:43 +0900

    댓글 작성 구현  진행중

--- routes/comments.js
+++ routes/comments.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require("../middlewares/auth-middleware");
 
-// 게시글 생성 API
-// 9. 토큰을 검사하여, 유효한 토큰일 경우에만 게시글 작성 가능
-router.post("/posts", authMiddleware, async (req, res) => {
-    // 게시글을 생성하는 사용자의 정보를 가지고 온다.
-    // 10. 제목, 작성 내용을 입력하기
+// 댓글 작성
+router.post("/posts/:postId/comment", authMiddleware, async (req, res) => {
     const { userId } = res.locals.user;
     const { title, content, nickname } = req.body;
     const post = await Posts.create({
@@ -13,7 +11,6 @@ router.post("/posts", authMiddleware, async (req, res) => {
         title, content, nickname
     });
 
-    // 201 = 생성
     return res.status(201).json({ data: post });
 });
 
