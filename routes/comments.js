b434b0ce3f09a99492e8b8e68d2cac1c7e3d8303
jiahnkim230502 <locker9417@gmail.com>
2023-06-27 15:05:18 +0900

    커밋

--- routes/comments.js
+++ routes/comments.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require("../middlewares/auth-middleware");
 
-// 댓글 작성
+// 댓글 작성 API
 router.post("/posts/:postId/comment", authMiddleware, async (req, res) => {
     const { userId } = res.locals.user;
     const { title, content, nickname } = req.body;
@@ -11,7 +11,17 @@ router.post("/posts/:postId/comment", authMiddleware, async (req, res) => {
         title, content, nickname
     });
 
-    return res.status(201).json({ data: post });
+    return res.status(201).json({ message: "게시글이 수정되었습니다." });
+});
+
+// 댓글 목록 조회 API
+router.get("/posts/:postId/comment", async (req, res) => {
+    const posts = await Posts.findAll({
+        attributes: ['userId','postId', 'title', 'nickname', 'createdAt', 'updatedAt'],
+        order: [['createdAt', 'DESC']],
+    });
+
+    return res.status(200).json({ data: posts });
 });
 
 module.exports = router;
