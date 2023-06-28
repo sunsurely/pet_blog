b2fc95f709fb14a19fa2d7e85244d4b8fcf4f817
Siwoo <qldrndlsrks1@gmail.com>
2023-06-28 12:35:19 +0900

    join

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const cookieParser = require('cookie-parser');
+const usersRouter = require("./routes/users");
 
 const app = express();
 const port = 3000;
@@ -8,7 +9,7 @@ const port = 3000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api', routes);
+app.use('/api', [routes, usersRouter]);
 app.use(express.static('public'));
 
 app.listen(port, () => {
