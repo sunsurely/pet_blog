b7144ef0fea9966e6e315645a97da2b851b72d3b
yeonoh <beky010101@gmail.com>
2023-06-29 16:19:44 +0900

    비번 암호화, 비번 암호화처리 된 것과 입력받은 것 확인 후 로그인, 내 정보 조회 구현

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { Users } = require('../models');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
+// 로그인 API
 router.post('/', async (req, res) => {
   const { nickname, password } = req.body;
 
@@ -10,10 +12,19 @@ router.post('/', async (req, res) => {
     where: { nickname },
   });
 
-  if (!user || password !== user.password) {
+  // 닉네임이 다른 경우
+  if (!user) {
+    return res.status(404).json({ errorMessage: '닉네임을 확인해주세요.' });
+  }
+
+  // 입력한 비밀번호가, 암호화 된 비밀번호와 일치하는지 확인
+  const passwordOk = await bcrypt.compare(password, user.password);
+  console.log(passwordOk);
+
+  if (!passwordOk) {
     return res
       .status(400)
-      .json({ errorMessage: '닉네임 또는 패스워드를 확인해주세요' });
+      .json({ errorMessage: '비밀번호가 일치하지 않습니다.' });
   }
 
   const token = jwt.sign({ usersId: user.usersId }, 'costomized-secret-key');
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const { Users } = require('../models');
+const loginMiddleware = require('../middleware/login-middleware.js');
 
+const bcrypt = require('bcrypt');
+const saltRounds = 15;
+
+// 내 정보 조회 API
+router.get('/me', loginMiddleware, async (req, res) => {
+  const { nickname } = res.locals.user;
+
+  res.status(200).json({
+    user: {
+      nickname: nickname,
+    },
+  });
+});
+
+// 회원가입 API
 router.post('/', async (req, res) => {
   const { nickname, password, confirm } = req.body;
   const pattern = /^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{3,}$/;
@@ -19,7 +35,7 @@ router.post('/', async (req, res) => {
   if (password !== confirm) {
     return res
       .status(412)
-      .json({ errorMessage: '패스워드가 패스워드 확인과 다릅니다.' });
+      .json({ errorMessage: '패스워드가 일치하지 않습니다.' });
   }
 
   const isExistUser = await Users.findOne({
@@ -28,13 +44,18 @@ router.post('/', async (req, res) => {
   if (isExistUser) {
     return res
       .status(412)
-      .json({ errorMessage: '이미 존재하는 이용자입니다.' });
+      .json({ errorMessage: '이미 존재하는 사용자입니다.' });
   }
 
-  const user = await Users.create({ nickname, password });
+  // 비밀번호 암호화 처리
+  const encrypted = await bcrypt.hash(password, saltRounds);
+
+  const user = await Users.create({
+    nickname,
+    password: encrypted, // 암호화 된 비밀번호를 저장
+  });
 
   return res.status(201).json({ message: '회원가입이 완료되었습니다.' });
 });
 
 module.exports = router;
-//
