16c184c20ca7b0e707912d8574427f4109af7aeb
Nam HyunJin <skaguswls200@naver.com>
2023-06-30 22:27:32 +0900

    fixUpdate

--- middleware/upload-middleware.js
+++ middleware/upload-middleware.js
@@ -34,8 +34,8 @@ const path = require('path');
 const s3Client = new S3Client({
   region: 'ap-northeast-2',
   credentials: {
-    accessKeyId: 'AKIA4XD5US3EDVGV5BUJ',
-    secretAccessKey: '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',
+    accessKeyId: '',
+    secretAccessKey: '',
   },
 });
 
